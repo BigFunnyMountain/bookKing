650d3df501ee0bb484eaf2b2da779dd502e3fbef
mixedsider <dmstjd0812@gmail.com>
2025-04-02 12:05:28 +0900

    init: 초기화
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

---
+++ src/main/java/xyz/tomorrowlearncamp/bookking/BookKingApplication.java
@@ -0,0 +1,13 @@
+package xyz.tomorrowlearncamp.bookking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/tomorrowlearncamp/bookking/BookKingApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tomorrowlearncamp.bookking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
