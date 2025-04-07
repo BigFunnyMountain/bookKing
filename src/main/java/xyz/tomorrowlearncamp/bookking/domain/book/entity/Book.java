89253c5f0374c6771c76ff745175f8076ec0cf05
mixedsider <dmstjd0812@gmail.com>
2025-04-07 11:37:02 +0900

    fix: book count -> stock
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/bookking/domain/book/entity/Book.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/domain/book/entity/Book.java
@@ -49,9 +49,7 @@ public class Book extends BaseEntity {
     private LocalDateTime publicationDate;
 
     @Column(nullable = false)
-    private Long count;
-
-    private Long stock;
+    private Long stock = 0L;
 
     @Builder
     public Book(Long bookId, String isbn, String title, String subject, String author, String publisher,
@@ -71,10 +69,7 @@ public class Book extends BaseEntity {
         this.stock = stock;
     }
 
-    public void updateStock(Long stock){
+    public void updateStock(Long stock) {
         this.stock = stock;
-        // 한권 구매
-    public void CountMinusOne() {
-        this.count = this.count - 1;
     }
 }
