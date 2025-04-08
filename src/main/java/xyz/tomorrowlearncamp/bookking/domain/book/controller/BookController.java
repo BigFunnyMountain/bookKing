6b928e59aa37d6f9cec8023367a66276f99751dd
mapleclover <51579720+mapleclover@users.noreply.github.com>
2025-04-08 10:37:21 +0900

    feat / library api

--- src/main/java/xyz/tomorrowlearncamp/bookking/domain/book/controller/BookController.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/domain/book/controller/BookController.java
@@ -1,11 +1,6 @@
 package xyz.tomorrowlearncamp.bookking.domain.book.controller;
 
-import java.util.List;
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-
-import org.hibernate.query.Page;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -21,11 +16,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import reactor.core.publisher.Mono;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import xyz.tomorrowlearncamp.bookking.domain.book.dto.request.AddBookRequestDto;
 import xyz.tomorrowlearncamp.bookking.domain.book.dto.request.SearchBookRequestDto;
-import xyz.tomorrowlearncamp.bookking.domain.book.dto.request.UpdateBookStockRequestDto;
 import xyz.tomorrowlearncamp.bookking.domain.book.dto.request.UpdateBookRequestDto;
+import xyz.tomorrowlearncamp.bookking.domain.book.dto.request.UpdateBookStockRequestDto;
 import xyz.tomorrowlearncamp.bookking.domain.book.dto.response.BookResponseDto;
 import xyz.tomorrowlearncamp.bookking.domain.book.dto.response.SearchBookResponseDto;
 import xyz.tomorrowlearncamp.bookking.domain.book.service.BookService;
@@ -35,60 +31,60 @@ import xyz.tomorrowlearncamp.bookking.domain.book.service.SearchBookService;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class BookController {
-    private final BookService bookService;
-    private final SearchBookService searchBookService;
+	private final BookService bookService;
+	private final SearchBookService searchBookService;
 
-    //ToDo : 국립도서관API 호출 Post
-    @PostMapping("/v1/books/search")
-    @ResponseStatus(HttpStatus.OK)
-    public SearchBookResponseDto searchBooks(@RequestBody SearchBookRequestDto requestDto){
-        return searchBookService.searchBooks(requestDto);
-    }
+	//ToDo : 국립도서관API 호출 Post
+	@PostMapping("/v1/books/search")
+	@ResponseStatus(HttpStatus.OK)
+	public SearchBookResponseDto searchBooks(@RequestBody SearchBookRequestDto requestDto) {
+		return searchBookService.searchBooks(requestDto);
+	}
 
-    //ToDo : 권한 체크 필요
-    @PostMapping("/v1/books")
-    public ResponseEntity<Long> addBook(@RequestBody @Valid AddBookRequestDto requestDto) {
-        return ResponseEntity.ok(bookService.addBook(requestDto));
-    }
+	//ToDo : 권한 체크 필요
+	@PostMapping("/v1/books")
+	public ResponseEntity<Long> addBook(@RequestBody @Valid AddBookRequestDto requestDto) {
+		return ResponseEntity.ok(bookService.addBook(requestDto));
+	}
 
-    //ToDo : 권한 체크 필요
-    @PatchMapping("/v1/books/{bookId}")
-    public ResponseEntity<Void> updateBook(
-        @PathVariable("bookId") Long bookId,
-        @RequestBody @Valid UpdateBookRequestDto requestDto
-    ){
-        bookService.updateBook(bookId, requestDto);
-        return ResponseEntity.ok().build();
-    }
+	//ToDo : 권한 체크 필요
+	@PatchMapping("/v1/books/{bookId}")
+	public ResponseEntity<Void> updateBook(
+		@PathVariable("bookId") Long bookId,
+		@RequestBody @Valid UpdateBookRequestDto requestDto
+	) {
+		bookService.updateBook(bookId, requestDto);
+		return ResponseEntity.ok().build();
+	}
 
-    //ToDo : 권한 체크 필요
-    @PatchMapping("/v1/books/{bookId}/stock")
-    public ResponseEntity<Void> updateBookStock(
-        @PathVariable("bookId") Long bookId,
-        @RequestBody @Valid UpdateBookStockRequestDto requestDto
-    ){
-        bookService.updateBookStock(bookId, requestDto);
-        return ResponseEntity.ok().build();
-    }
+	//ToDo : 권한 체크 필요
+	@PatchMapping("/v1/books/{bookId}/stock")
+	public ResponseEntity<Void> updateBookStock(
+		@PathVariable("bookId") Long bookId,
+		@RequestBody @Valid UpdateBookStockRequestDto requestDto
+	) {
+		bookService.updateBookStock(bookId, requestDto);
+		return ResponseEntity.ok().build();
+	}
 
-        //ToDo : 권한 체크 필요
-    @DeleteMapping("/v1/books/{bookId}")
-    public ResponseEntity<Void> deleteBook(@PathVariable("bookId") Long bookId){
-        bookService.deleteBook(bookId);
-        return ResponseEntity.ok().build();
-    }
+	//ToDo : 권한 체크 필요
+	@DeleteMapping("/v1/books/{bookId}")
+	public ResponseEntity<Void> deleteBook(@PathVariable("bookId") Long bookId) {
+		bookService.deleteBook(bookId);
+		return ResponseEntity.ok().build();
+	}
 
-    @GetMapping("/v1/books")
-    public ResponseEntity<Page<BookResponseDto>> getAllBooks(
-        @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "10") int size
-    ){
-        Pageable pageable = PageRequest.of(page, size);
-        return ResponseEntity.ok(bookService.getAllBooks(pageable));
-    }
+	@GetMapping("/v1/books")
+	public ResponseEntity<Page<BookResponseDto>> getAllBooks(
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size
+	) {
+		Pageable pageable = PageRequest.of(page, size);
+		return ResponseEntity.ok(bookService.getAllBooks(pageable));
+	}
 
-    @GetMapping("/v1/books/{bookId}")
-    public ResponseEntity<BookResponseDto> getBookById(@PathVariable("bookId") Long bookId){
-        return ResponseEntity.ok(bookService.getBookById(bookId));
-    }
+	@GetMapping("/v1/books/{bookId}")
+	public ResponseEntity<BookResponseDto> getBookById(@PathVariable("bookId") Long bookId) {
+		return ResponseEntity.ok(bookService.getBookById(bookId));
+	}
 }
