330070ca9aff7041fef4922891beb1c4b9d6db0d
polaris65b <polaris65b@gmail.com>
2025-04-24 19:36:59 +0900

    refactor: url 수정

--- src/main/java/xyz/tomorrowlearncamp/bookking/domain/keyword/controller/KeywordController.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/domain/keyword/controller/KeywordController.java
@@ -11,17 +11,17 @@ import xyz.tomorrowlearncamp.bookking.domain.keyword.service.KeywordService;
 import xyz.tomorrowlearncamp.bookking.domain.user.auth.dto.AuthUser;
 
 @RestController
-@RequestMapping("/api/keywords")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class KeywordController {
     private final KeywordService keywordService;
 
-    @PostMapping("/suggest")
+    @PostMapping("/keywords/suggest")
     public ResponseEntity<KeywordResponse> suggestKeywords(@Valid @RequestBody KeywordRequest request) {
         return ResponseEntity.ok(keywordService.suggestKeywords(request.getSearchTerm()));
     }
 
-    @GetMapping("/recommendations")
+    @GetMapping("/keywords/recommendations")
     public ResponseEntity<KeywordResponse> getPersonalizedRecommendations(
             @AuthenticationPrincipal AuthUser authUser) {
         return ResponseEntity.ok(keywordService.suggestByOrder(authUser.getUserId()));
