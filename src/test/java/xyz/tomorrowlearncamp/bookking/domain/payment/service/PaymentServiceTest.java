be25181c1995fe043f7138bcfe04510e28c49626
mixedsider <dmstjd0812@gmail.com>
2025-05-03 23:26:59 +0900

    test: payments 테스트 추가
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/test/java/xyz/tomorrowlearncamp/bookking/domain/payment/service/PaymentServiceTest.java
+++ src/test/java/xyz/tomorrowlearncamp/bookking/domain/payment/service/PaymentServiceTest.java
@@ -65,7 +65,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 구매 성공 테스트")
-	void paymentSingleSuccessTest() throws InterruptedException {
+	void paymentSuccessTest() throws InterruptedException {
 		//given
 		UserResponse user = new UserResponse();
 		ReflectionTestUtils.setField(user, "id", 1L);
@@ -95,7 +95,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 구매 실패 테스트 : 존재 하지 않는 책")
-	void paymentSingleFailedTest1() throws InterruptedException {
+	void paymentFailedTest1() throws InterruptedException {
 		//given
 		given(bookRepository.findById(anyLong())).willReturn(Optional.empty());
 		given(redissonClient.getFairLock(anyString())).willReturn(rlock);
@@ -111,7 +111,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 구매 실패 테스트 : 책 권수 없음")
-	void paymentSingleFailedTest2() throws InterruptedException {
+	void paymentFailedTest2() throws InterruptedException {
 		//given
 		Book book = new Book();
 		ReflectionTestUtils.setField(book, "bookId", 1L);
@@ -132,7 +132,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 구매 실패 테스트 : 사용자의 돈 없음")
-	void paymentSingleFailedTest3() throws InterruptedException {
+	void paymentFailedTest3() throws InterruptedException {
 		//given
 		Book book = new Book();
 		ReflectionTestUtils.setField(book, "bookId", 1L);
@@ -152,97 +152,8 @@ class PaymentServiceTest {
 	}
 
 	@Test
-	@DisplayName("멀티 쓰래드 v1 구매 성공 테스트")
-	void paymentV1MutiSuccessTest() throws InterruptedException {
-		//given
-		UserResponse user = new UserResponse();
-		ReflectionTestUtils.setField(user, "id", 1L);
-		ReflectionTestUtils.setField(user, "email", "test@test.com");
-		ReflectionTestUtils.setField(user, "role", UserRole.ROLE_USER);
-
-		Book book = new Book();
-		ReflectionTestUtils.setField(book, "bookId", 1L);
-		ReflectionTestUtils.setField(book, "stock", 100L);
-		ReflectionTestUtils.setField(book, "prePrice", "1");
-
-		int threadCount = 100;
-		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-		CountDownLatch latch = new CountDownLatch(threadCount);
-
-		given(bookRepository.findByIdWithLock(anyLong())).willReturn(book);
-		given(redissonClient.getFairLock(anyString())).willReturn(rlock);
-		given(rlock.tryLock(100L, 10L, TimeUnit.SECONDS)).willReturn(true);
-
-		//when
-		for (int i = 0; i < threadCount; i++) {
-			Long userId = (long)i;
-			executorService.execute(() -> {
-				try {
-					paymentService.paymentV1(userId, 1L, 1L, 1L, PayType.KAKAO_PAY);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				} finally {
-					latch.countDown();
-				}
-			});
-		}
-
-		latch.await();
-		executorService.shutdown();
-
-		// then
-		assertEquals(0, book.getStock());
-	}
-
-
-
-	@Test
-	@DisplayName("멀티 쓰래드 v2 구매 성공 테스트")
-	void paymentV2MutiSuccessTest() throws InterruptedException {
-		//given
-		UserResponse user = new UserResponse();
-		ReflectionTestUtils.setField(user, "id", 1L);
-		ReflectionTestUtils.setField(user, "email", "test@test.com");
-		ReflectionTestUtils.setField(user, "role", UserRole.ROLE_USER);
-
-		Book book = new Book();
-		ReflectionTestUtils.setField(book, "bookId", 1L);
-		ReflectionTestUtils.setField(book, "stock", 100L);
-		ReflectionTestUtils.setField(book, "prePrice", "1");
-
-		int threadCount = 100;
-		ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-		CountDownLatch latch = new CountDownLatch(threadCount);
-
-		given(bookRepository.findById(anyLong())).willReturn(Optional.of(book));
-		given(redissonClient.getFairLock(anyString())).willReturn(rlock);
-		given(rlock.tryLock(100L, 10L, TimeUnit.SECONDS)).willReturn(true);
-
-		//when
-		for (int i = 0; i < threadCount; i++) {
-			Long userId = (long)i;
-			executorService.execute(() -> {
-				try {
-					paymentService.paymentV2(userId, 1L, 1L, 1L, PayType.KAKAO_PAY);
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				} finally {
-					latch.countDown();
-				}
-			});
-		}
-
-		latch.await();
-		executorService.shutdown();
-
-		// then
-		verify(bookRepository, times(100)).save(any(book.getClass()));
-		assertEquals(0, book.getStock());
-	}
-
-	@Test
 	@DisplayName("단독 환불 성공 테스트")
-	void returnPaymentSingleSuccessTest() throws InterruptedException {
+	void returnPaymentSuccessTest() throws InterruptedException {
 		//given
 		UserResponse user = new UserResponse();
 		ReflectionTestUtils.setField(user, "id", 1L);
@@ -282,7 +193,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 환불 실패 테스트 : 오더의 유저와 같은 유저가 아님")
-	void returnPaymentSingleFailedTest1() {
+	void returnPaymentFailedTest1() {
 		//given
 		UserResponse user = new UserResponse();
 		ReflectionTestUtils.setField(user, "id", 1L);
@@ -315,7 +226,7 @@ class PaymentServiceTest {
 
 	@Test
 	@DisplayName("단독 환불 실패 테스트 : 오더의 유저와 같은 유저가 아님")
-	void returnPaymentSingleFailedTest2() {
+	void returnPaymentFailedTest2() throws InterruptedException {
 		//given
 		UserResponse user = new UserResponse();
 		ReflectionTestUtils.setField(user, "id", 1L);
@@ -323,7 +234,7 @@ class PaymentServiceTest {
 		ReflectionTestUtils.setField(user, "role", UserRole.ROLE_USER);
 
 		Order order = Order.builder()
-				.userId(2L)
+				.userId(1L)
 				.bookId(1L)
 				.prePrice("15000")
 				.stock(3L)
@@ -334,13 +245,15 @@ class PaymentServiceTest {
 		ReflectionTestUtils.setField(order, "orderId", 1L);
 
 		given(orderService.getOrder(anyLong())).willReturn(OrderResponse.of(order));
+		given(redissonClient.getFairLock(anyString())).willReturn(rlock);
+		given(rlock.tryLock(100L, 10L, TimeUnit.SECONDS)).willReturn(true);
 
 		// when && then
 		try {
 			paymentService.returnPayment(user.getId(), order.getOrderId());
-		} catch (InvalidRequestException e) {
-			assertInstanceOf(InvalidRequestException.class, e);
-			assertEquals(ErrorMessage.NO_AUTHORITY_TO_RETURN_A_PAYMENT, e.getErrorMessage());
+		} catch (NotFoundException e) {
+			assertInstanceOf(NotFoundException.class, e);
+			assertEquals(ErrorMessage.BOOK_NOT_FOUND, e.getErrorMessage());
 			return;
 		}
 		fail();
