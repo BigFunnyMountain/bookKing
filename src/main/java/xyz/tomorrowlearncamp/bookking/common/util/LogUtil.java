2dee221345745ca124e68914aa4ae089c861d770
mixedsider <dmstjd0812@gmail.com>
2025-05-04 20:12:19 +0900

    fix : logUtil 수정
    Signed-off-by: mixedsider <dmstjd0812@gmail.com>

--- src/main/java/xyz/tomorrowlearncamp/bookking/common/util/LogUtil.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/common/util/LogUtil.java
@@ -35,11 +35,8 @@ public class LogUtil {
 
 	public static String getAgeGroup(int age) {
 		if (age < 10) return "10대 미만";
-		if (age < 20) return "10대";
-		if (age < 30) return "20대";
-		if (age < 40) return "30대";
-		if (age < 50) return "40대";
-		if (age < 60) return "50대";
-		return "60대 이상";
+		if (age >= 60) return "60대 이상";
+		return (age / 10 * 10) + "대";
+
 	}
 }
