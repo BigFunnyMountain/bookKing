d798d7078db1224c2b2ece22ae3d4bbc78889c66
dereck-jun <yjpark0805@naver.com>
2025-05-04 09:31:12 +0900

    fix: UserRole.of() 메서드 수정 및 BookService 필드 수정

--- src/main/java/xyz/tomorrowlearncamp/bookking/common/enums/ErrorMessage.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/common/enums/ErrorMessage.java
@@ -18,6 +18,7 @@ public enum ErrorMessage {
 	INVALID_JWT_SIGNATURE(BAD_REQUEST,"유효하지 않는 JWT 서명 입니다."),
 	UNSUPPORTED_JWT_TOKEN(BAD_REQUEST, "지원되지 않는 JWT 토큰 입니다."),
 	EXPIRED_JWT_REFRESH_TOKEN(BAD_REQUEST, "다시 로그인 해주세요."),
+	INVALID_USER_ROLE(BAD_REQUEST, "유효하지 않는 역할입니다."),
 
     NO_AUTHORITY_TO_WRITE_A_REVIEW(FORBIDDEN, "리뷰를 작성할 권한이 없습니다."),
     INVALID_HEADER(BAD_REQUEST, "Authorization 헤더가 잘못되었습니다."),
--- src/main/java/xyz/tomorrowlearncamp/bookking/domain/user/enums/UserRole.java
+++ src/main/java/xyz/tomorrowlearncamp/bookking/domain/user/enums/UserRole.java
@@ -1,27 +1,25 @@
 package xyz.tomorrowlearncamp.bookking.domain.user.enums;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Arrays;
+import xyz.tomorrowlearncamp.bookking.common.enums.ErrorMessage;
+import xyz.tomorrowlearncamp.bookking.common.exception.InvalidRequestException;
 
 /**
- * 작성자 : 문성준
- * 일시 : 2025.04.03 - v1
+ * 작성자 : 문성준 일시 : 2025.04.03 - v1
  */
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-    ROLE_USER,
-    ROLE_ADMIN;
+	ROLE_USER,
+	ROLE_ADMIN;
 
-    public static UserRole of(String userRole) {
-        String roleChange = userRole.startsWith("ROLE_") ? userRole : "ROLE_" + userRole;
-
-        return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(roleChange))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("유효하지 않은 UserRole 입니다."));
-    }
+	public static UserRole of(String input) {
+		for (UserRole role : values()) {
+			if (role.name().equalsIgnoreCase(input)) {
+				return role;
+			}
+		}
+		throw new InvalidRequestException(ErrorMessage.INVALID_USER_ROLE);
+	}
 }
